d6d511913a681d41183837dc662242aabe3f3e1d
NikitaBosh <nrandryuha@mail.ru>
2021-05-17 10:13:23 +0300

    Created seed users and tasks

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password,
             'remember_token' => Str::random(10),
         ];
     }
@@ -44,4 +44,20 @@ class UserFactory extends Factory
             ];
         });
     }
+        /**
+     * Состояние для тестового пользователя
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function test()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'test',
+                'email' => 'test@test.ru',
+                'password' => bcrypt('123456'),
+            ];
+        });
+    }
+
 }
